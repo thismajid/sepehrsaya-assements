b7ef4935700aa14589993bb1b32c01890db28140
Majid Khorshidian <mkhorshidian72@gmail.com>
2024-07-04 21:51:28 +0330

    Add user model unit test

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
 const { toJSON, paginate } = require("./plugins");
@@ -17,8 +18,29 @@ const usersSchema = mongoose.Schema(
       type: String,
       required: true,
       unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email");
+        }
+      },
     },
-    password: { type: String, min: 6, required: true },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+      validate(value) {
+        if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+          throw new Error(
+            "Password must contain at least one letter and one number"
+          );
+        }
+      },
+      private: true, // used by the toJSON plugin
+    },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
   },
   {
     timestamps: true,
